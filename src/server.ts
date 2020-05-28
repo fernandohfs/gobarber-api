e7690c69c9a5c0d1f7affbb8042fcb106f68f131
Fernando Soares <fernando.soares@luizalabs.com>
2020-05-28 15:41:44 -0300

    :sparkles: Add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
+
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -9,6 +11,7 @@ import handleErrors from './middlewares/handleErrors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
